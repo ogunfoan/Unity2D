4acc1e5cc99f37553212fbe4909e262fa181ead3
ogunfoan <64503396+ogunfoan@users.noreply.github.com>
2024-08-06 01:02:02 +0300

    engellere yeni kod eklendi

--- Assets/Scripts/Engeller.cs
+++ Assets/Scripts/Engeller.cs
@@ -1,18 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Engeller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private SceneManager _sceneManager;
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.CompareTag("Engel"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Destroy(other.gameObject);
+        }
     }
 }
