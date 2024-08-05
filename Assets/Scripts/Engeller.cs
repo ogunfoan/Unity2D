90663afcf71afb6ebcfd91bf2a56cfbf2570f45e
ogunfoan <64503396+ogunfoan@users.noreply.github.com>
2024-08-06 01:39:47 +0300

    cok iyi

--- Assets/Scripts/Engeller.cs
+++ Assets/Scripts/Engeller.cs
@@ -7,12 +7,12 @@ using UnityEngine.SceneManagement;
 public class Engeller : MonoBehaviour
 {
     private SceneManager _sceneManager;
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Engel"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             Destroy(other.gameObject);
+            ToplamVeriler.Eksican++;
         }
     }
 }
--- Assets/Scripts/Toplanabilirler.cs
+++ Assets/Scripts/Toplanabilirler.cs
@@ -2,19 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Toplanabilirler : MonoBehaviour
 {
-    private int say = 0;
     [SerializeField] private AudioClip ToplamaSesi;
+    [SerializeField] public TextMeshPro _text;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Kek"))
         {
             AudioSource.PlayClipAtPoint(ToplamaSesi, other.transform.position);
-            say++; 
+            ToplamVeriler.Skor++; 
             Destroy(other.gameObject);
+            _text.text = ToplamVeriler.Skor.ToString();
         }
     }
 }
