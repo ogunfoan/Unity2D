6bc297f919a3fc6259709f4e67c2c1d8941ac1b9
ogunfoan <ogunfoan@hotmail.com>
2024-08-06 23:03:05 +0300

    Sorunlar düzeltildi
    -Ölmene neden olan keklerin ekrana yansıması düzeltildi.

--- Assets/Scripts/ToplamVeriler.cs
+++ Assets/Scripts/ToplamVeriler.cs
@@ -5,6 +5,5 @@ using UnityEngine;
 public class ToplamVeriler : MonoBehaviour
 {
    public static int Can = 3;
-   public static int Eksican = 3;
    public static int Skor=0;
 }
--- Assets/Scripts/ZehirSeviyesi.cs
+++ Assets/Scripts/ZehirSeviyesi.cs
@@ -2,23 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-
 public class ZehirSeviyesi : MonoBehaviour
 {
-    private SceneManager _sceneManager;
-    private void Awake()
+    private void Update()
     {
-        switch (ToplamVeriler.Eksican)
+        switch (ToplamVeriler.Can)
         {
             case 3:
                 break;
             case 2:
-                gameObject.transform.GetChild(2).gameObject.SetActive(false);
+                gameObject.transform.GetChild(0).gameObject.SetActive(true);
                 break;
             case 1:
-                gameObject.transform.GetChild(2).gameObject.SetActive(false);
-                gameObject.transform.GetChild(1).gameObject.SetActive(false);
+                gameObject.transform.GetChild(0).gameObject.SetActive(true);
+                gameObject.transform.GetChild(1).gameObject.SetActive(true);
                 break;
             case 0:
                 SceneManager.LoadScene(2);
